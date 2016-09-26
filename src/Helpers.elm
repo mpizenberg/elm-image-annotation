13be5d36e161b2de3a7210fe48a7c318be830c16
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2016-09-26 18:38:04 +0800

    Add functions in Helpers module mouse events.

--- src/Helpers.elm
+++ src/Helpers.elm
@@ -1,10 +1,49 @@
 module Helpers exposing
     (..)
 
+
+import Html as H
+import Html.Events as HE
+import Json.Decode as J
 import Task
+
+
+import DOM
+
+
 
 
 {-| Transform a message to a Cmd message -}
 msgToCmd : msg -> Cmd msg
 msgToCmd message =
     Task.perform identity identity (Task.succeed message)
+
+
+
+
+-- ATTRIBUTE MSG ON MOUSE EVENTS #####################################
+
+
+
+
+floatToInt : (Float, Float) -> (Int, Int)
+floatToInt (x,y) = (round x, round y)
+
+
+{-| Get the offsetX and offsetY properties of a mouse event -}
+offsetOn : String -> (((Int,Int) -> msg) -> H.Attribute msg)
+offsetOn mouseEvent =
+    specialOn mouseEvent DOM.offset floatToInt
+
+
+{-| Generic function to get results from transformed properties at a mouse event.
+It uses a decoder and a function transforming the decoder results.
+-}
+specialOn : String -> J.Decoder a -> (a -> b) -> ((b -> msg) -> H.Attribute msg)
+specialOn mouseEvent decoder transform =
+    \tagger ->
+        HE.onWithOptions
+            mouseEvent
+            {stopPropagation=True, preventDefault=True}
+            (J.map (tagger << transform) decoder)
+
