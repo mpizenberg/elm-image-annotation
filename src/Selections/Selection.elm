1bb984eadc4f1c0ed52ec2b6df4d5ae33c94546b
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2016-11-09 14:19:11 +0800

    Light cleaning and formating of Selection.elm.

--- src/Selections/Selection.elm
+++ src/Selections/Selection.elm
@@ -2,24 +2,19 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-module Selections.Selection exposing (..)
 
+module Selections.Selection exposing (..)
 
 {-| The Selection module aims at regrouping functions useful to all kind of selections
 -}
 
-
-import Svg
+import Svg exposing (Svg)
 import Svg.Attributes as SvgA
 import Json.Encode as JE
 import Time exposing (Time)
 
 
-
-
 -- MODEL #############################################################
-
-
 
 
 type alias Pos =
@@ -43,10 +38,14 @@ type alias Style =
 
 defaultStyle : Style
 defaultStyle =
-    Style "red" 3 False
+    { color = "red"
+    , strokeWidth = 3
+    , highlighted = False
+    }
 
 
-type alias Timings = ( Maybe Time, Maybe Time )
+type alias Timings =
+    ( Maybe Time, Maybe Time )
 
 
 type alias Selection =
@@ -57,140 +56,134 @@ type alias Selection =
 
 
 defaultSelection : Selection
-defaultSelection = Selection defaultStyle (Nothing, Nothing) False
-
+defaultSelection =
+    { style = defaultStyle
+    , timings = ( Nothing, Nothing )
+    , pointerEvents = False
+    }
 
 
 
 -- UPDATE ############################################################
 
 
+updateStartTime : Maybe Time -> Selection -> Selection
+updateStartTime t selection =
+    { selection | timings = ( t, snd selection.timings ) }
 
 
-changeStyle : Maybe String -> Maybe Int -> Maybe Bool -> Style -> Style
-changeStyle color strokeWidth highlighted oldStyle =
-    let
-        color' = Maybe.withDefault oldStyle.color color
-        strokeWidth' = Maybe.withDefault oldStyle.strokeWidth strokeWidth
-        highlighted' = Maybe.withDefault oldStyle.highlighted highlighted
-    in
-        Style color' strokeWidth' highlighted'
-
-
-changeSelectionStyle : Maybe String -> Maybe Int -> Maybe Bool -> Selection -> Selection
-changeSelectionStyle color strokeWidth highlighted selection =
-    { selection | style = changeStyle color strokeWidth highlighted selection.style }
-
-
-startTime : Maybe Time -> Timings -> Timings
-startTime t timings = ( t, snd timings )
-
-
-stopTime : Maybe Time -> Timings -> Timings
-stopTime t timings = ( fst timings, t )
-
-
-startSelectionTime : Maybe Time -> Selection -> Selection
-startSelectionTime t selection =
-    { selection | timings = startTime t selection.timings }
-
-
-stopSelectionTime : Maybe Time -> Selection -> Selection
-stopSelectionTime t selection =
-    { selection | timings = stopTime t selection.timings }
+updateStopTime : Maybe Time -> Selection -> Selection
+updateStopTime t selection =
+    { selection | timings = ( fst selection.timings, t ) }
 
 
 resetTimings : Selection -> Selection
 resetTimings selection =
-    { selection | timings = (Nothing, Nothing) }
-
+    { selection | timings = ( Nothing, Nothing ) }
 
 
 
 -- VIEW ##############################################################
 
 
-
-
 selectionAttributes : Selection -> List (Svg.Attribute msg)
 selectionAttributes selection =
-    ( SvgA.pointerEvents <| if selection.pointerEvents then "auto" else "none" )
-    :: ( styleAttributes selection.style )
+    pointerEventsAttribute selection.pointerEvents
+        :: (styleAttributes selection.style)
+
+
+pointerEventsAttribute : Bool -> Svg.Attribute msg
+pointerEventsAttribute pointerEvents =
+    SvgA.pointerEvents <|
+        if pointerEvents then
+            "auto"
+        else
+            "none"
 
 
 styleAttributes : Style -> List (Svg.Attribute msg)
 styleAttributes style =
-    let
-        opacityAttr = if style.highlighted then "0.5" else "0"
-    in
-        [ SvgA.stroke style.color
-        , SvgA.fill style.color
-        , SvgA.strokeWidth (toString style.strokeWidth)
-        , SvgA.fillOpacity opacityAttr
-        ]
+    [ SvgA.stroke style.color
+    , SvgA.fill style.color
+    , SvgA.strokeWidth (toString style.strokeWidth)
+    , SvgA.fillOpacity <| opacityAttr style.highlighted
+    ]
 
+
+opacityAttr : Bool -> String
+opacityAttr highlighted =
+    if highlighted then
+        "0.5"
+    else
+        "0"
 
 
 
 -- OUTPUTS ##############################################################
 
 
-
-
 maybeTimeObject : Maybe Time -> JE.Value
 maybeTimeObject maybeTime =
     case maybeTime of
-        Nothing -> JE.null
-        Just time -> JE.float time
+        Nothing ->
+            JE.null
+
+        Just time ->
+            JE.float time
 
 
 duration : Timings -> Maybe Time
 duration timings =
     case timings of
-        (Nothing, _) -> Nothing
-        (_, Nothing) -> Nothing
-        (Just start, Just stop) -> Just (stop - start)
+        ( Nothing, _ ) ->
+            Nothing
+
+        ( _, Nothing ) ->
+            Nothing
+
+        ( Just start, Just stop ) ->
+            Just (stop - start)
 
 
 posObject : Pos -> JE.Value
 posObject pos =
     JE.object
-        [ ("x", JE.int pos.x)
-        , ("y", JE.int pos.y)
+        [ ( "x", JE.int pos.x )
+        , ( "y", JE.int pos.y )
         ]
 
 
 posPathObject : Pos -> JE.Value
 posPathObject pos =
-    JE.list [JE.int pos.x, JE.int pos.y]
+    JE.list [ JE.int pos.x, JE.int pos.y ]
 
 
 sizeObject : Size -> JE.Value
 sizeObject size =
     JE.object
-        [ ("width", JE.int size.width)
-        , ("height", JE.int size.height)
+        [ ( "width", JE.int size.width )
+        , ( "height", JE.int size.height )
         ]
 
 
 selectionObject : Selection -> JE.Value
 selectionObject selection =
     JE.object
-        [ ("style", styleObject selection.style)
-        , ("timings", timingsObject selection.timings)
-        , ("pointerEvents", JE.bool selection.pointerEvents)
+        [ ( "style", styleObject selection.style )
+        , ( "timings", timingsObject selection.timings )
+        , ( "pointerEvents", JE.bool selection.pointerEvents )
         ]
 
 
 timingsObject : Timings -> JE.Value
-timingsObject (start, stop) =
-    JE.list <| List.map maybeTimeObject [start, stop]
+timingsObject ( start, stop ) =
+    JE.list <| List.map maybeTimeObject [ start, stop ]
 
 
 styleObject : Style -> JE.Value
 styleObject style =
     JE.object
-        [ ("color", JE.string style.color)
-        , ("strokeWidth", JE.int style.strokeWidth)
-        , ("highlighted", JE.bool style.highlighted)
+        [ ( "color", JE.string style.color )
+        , ( "strokeWidth", JE.int style.strokeWidth )
+        , ( "highlighted", JE.bool style.highlighted )
         ]
