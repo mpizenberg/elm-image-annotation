37145305dc947379d21fb6fb60fe1e287d9be30e
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2016-11-09 15:13:05 +0800

    Change RectangleSeleciton -> Rectangle.

--- src/Selections/Rectangle.elm
+++ src/Selections/Rectangle.elm
@@ -3,7 +3,7 @@
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 
-module Selections.RectangleSelection exposing (..)
+module Selections.Rectangle exposing (..)
 
 {-| RectangleSelection contains the tools to manipule rectangle selections.
 -}
