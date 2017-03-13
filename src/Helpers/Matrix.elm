0938c9d4ce743d47c46491327ccbc0224f947890
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2017-03-13 13:03:50 +0800

    Add a Matrix decoder and encoder.

---
+++ src/Helpers/Matrix.elm
@@ -0,0 +1,30 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+
+module Helpers.Matrix exposing (..)
+
+import Matrix exposing (Matrix)
+import Json.Encode as Encode
+import Json.Decode as Decode exposing (Decoder)
+import Array
+
+
+encode : Matrix Bool -> Encode.Value
+encode { size, data } =
+    Encode.object
+        [ ( "width", Tuple.first size |> Encode.int )
+        , ( "height", Tuple.second size |> Encode.int )
+        , ( "data", data |> Array.map Encode.bool |> Encode.array )
+        ]
+
+
+decode : Decoder (Matrix Bool)
+decode =
+    Decode.map2 Matrix
+        (Decode.map2 (,)
+            (Decode.field "width" Decode.int)
+            (Decode.field "height" Decode.int)
+        )
+        (Decode.field "data" <| Decode.array Decode.bool)
