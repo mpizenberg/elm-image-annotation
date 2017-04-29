d34012c2f696630a464ddceea27b0431ec4d9023
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2017-04-29 18:46:55 +0800

    Start with the Types module

---
+++ src/Annotation/Types.elm
@@ -0,0 +1,51 @@
+module Annotation.Types
+    exposing
+        ( Point
+        , BoundingBox
+        , Contour
+        , Outline
+        , Scribble
+        )
+
+{-| Types of the different elements usable for annotations.
+
+In order to initialize an object of one of these types,
+look into the corresponding module.
+For example, to initialize a `Point`,
+you will use the `Point.fromCoordinates` function from the `Point` module.
+
+@docs Point, BoundingBox, Contour, Outline, Scribble
+
+-}
+
+import Annotation.TypesPrivate as Private
+
+
+{-| A point.
+-}
+type alias Point =
+    Private.Point
+
+
+{-| A bounding box.
+-}
+type alias BoundingBox =
+    Private.BoundingBox
+
+
+{-| A contour.
+-}
+type alias Contour =
+    Private.Contour
+
+
+{-| An outline.
+-}
+type alias Outline =
+    Private.Outline
+
+
+{-| A scribble.
+-}
+type alias Scribble =
+    Private.Scribble
---
+++ src/Annotation/TypesPrivate.elm
@@ -0,0 +1,23 @@
+module Annotation.TypesPrivate exposing (..)
+
+import OpenSolid.Geometry.Types exposing (..)
+
+
+type alias Point =
+    Point2d
+
+
+type alias BoundingBox =
+    BoundingBox2d
+
+
+type alias Contour =
+    Polygon2d
+
+
+type alias Outline =
+    Polygon2d
+
+
+type alias Scribble =
+    Polyline2d
