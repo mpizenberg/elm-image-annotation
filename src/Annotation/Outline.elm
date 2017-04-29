8f4dc4e85abd8f0122cfbaa304cdac441980539e
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2017-04-30 00:30:34 +0800

    Start the Outline module

---
+++ src/Annotation/Outline.elm
@@ -0,0 +1,30 @@
+module Annotation.Outline
+    exposing
+        ( empty
+        , addPoint
+        )
+
+{-| Create and manipulate freeline closed outlines.
+
+@docs empty, addPoint
+
+-}
+
+import Annotation.Types exposing (..)
+import OpenSolid.Geometry.Types exposing (..)
+
+
+{-| Create an empty outline.
+-}
+empty : Outline
+empty =
+    Polygon2d []
+
+
+{-| Add a point to an unfinished outline.
+-}
+addPoint : Point -> Outline -> Outline
+addPoint point outline =
+    case outline of
+        Polygon2d outlinePoints ->
+            Polygon2d (point :: outlinePoints)
