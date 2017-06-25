82e6afcb109b14b63f4cda9df4c6714738077afa
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2017-06-26 00:27:27 +0800

    Add a default viewer size

--- src/Annotation/Viewer.elm
+++ src/Annotation/Viewer.elm
@@ -2,11 +2,12 @@ module Annotation.Viewer exposing (..)
 
 {-| This module provides functions to manage the viewing area.
 
-@docs Size, Viewer
+@docs Size, Viewer, default
 
 -}
 
 import OpenSolid.Geometry.Types exposing (..)
+import OpenSolid.Frame2d as Frame2d
 
 
 {-| Size is a type alias for a pair of floats ( width, height ).
@@ -21,4 +22,14 @@ type alias Viewer =
     { frame : Frame2d
     , size : Size
     , zoom : Float
+    }
+
+
+{-| Default viewer.
+-}
+default : Viewer
+default =
+    { frame = Frame2d.xy
+    , size = ( 800, 400 )
+    , zoom = 1
     }
