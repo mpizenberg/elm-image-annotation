746449b98679767030bd5884601dc6efbd5de40e
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2017-10-18 17:30:56 +0800

    Update point

--- src/Annotation/Geometry/Point.elm
+++ src/Annotation/Geometry/Point.elm
@@ -10,11 +10,11 @@ module Annotation.Geometry.Point
 -}
 
 import Annotation.Geometry.Types exposing (..)
-import OpenSolid.Geometry.Types exposing (..)
+import OpenSolid.Point2d as Point2d exposing (Point2d)
 
 
 {-| Create a Point with the given coordinates.
 -}
 fromCoordinates : ( Float, Float ) -> Point
-fromCoordinates coordinates =
-    Point2d coordinates
+fromCoordinates =
+    Point2d.fromCoordinates
